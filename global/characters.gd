ff38d70f015d10f5d23a30973588b553089323b0
Alan <alanmel97@gmail.com>
2026-02-13 21:56:58 +0100

    finally skin saving works!!!!

--- global/characters.gd
+++ global/characters.gd
@@ -5,15 +5,16 @@ extends Node
 @export var skins: Array[CharacterSkin]
 
 
-#func _ready() -> void:
-## Get skins from characters. this is only used before any saves, after skins will be overriden by loader
-#for character in Characters.characters:
-#if character.skin != null and character.skin not in skins:
-#skins.append(character.skin)
-## now the skins resources are linked to the ones saved on //res. I have to make them unique
-#skins = skins.duplicate(true) #make skins have unique resources
-#print(skins[0])
-#print(skins[6])
+func _ready() -> void:
+	# Get skins from characters. this is only used before any saves, after skins will be overriden by loader
+	for character in Characters.characters:
+		if character.skin != null and character.skin not in skins:
+			skins.append(character.skin)
+	# now the skins resources are linked to the ones saved on //res. I have to make them unique
+	skins = skins.duplicate_deep() #make skins have unique resources
+	print(skins[0])
+
+
 func get_current_rival() -> Character:
 	for c in characters:
 		if not c.beated:
